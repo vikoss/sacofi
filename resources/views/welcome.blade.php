da86d42d4d7ce0133311ada1a781216fb7b71af3
Victor <victor.torrecillas@globalcorporation.cc>
2020-01-23 09:54:50 -0600

    project start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <!-- Scripts -->
+
+        <script src="{{ asset('js/app.js') }}"></script>
+
+        
+    </head>
+    <body>
+
+        <header>
+
+        </header>
+        <main>
+            
+        </main>
+
+    </body>
+</html>
